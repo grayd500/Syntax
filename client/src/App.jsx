9cc69feac931241e84c8b13f875a3a7aaa8b9c3b
tugwellchristi <tugwellchristi@gmail.com>
2024-01-19 10:00:36 -0800

    Removed initial src folder & contents, replaced with Dale's, added component folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Navbar from "../src/components/Navbar/Navbar.jsx";
+import About from "../src/components/About/About.jsx";
+import Contact from "../src/components/Contact/Contact.jsx";
+import Discography from "../src/components/Discography/Discography.jsx";
+import Merch from "../src/components/Merch/Merch.jsx";
+import VIP from "../src/components/VIP/VIP.jsx";
+import Footer from "../src/components/Footer/Footer.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Merch />
+      <Discography />
+      <Contact />
+      <VIP />
+      <Footer />
+    </main>
+  );
+  }
