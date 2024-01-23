b264ea37157432a8ee35f2513232f901ca01de8f
dmod129 <derek.modugno@gmail.com>
2024-01-22 20:18:50 -0600

    created user seeds

---
+++ server/seeds/userSeed.js
@@ -0,0 +1,22 @@
+[
+    {
+        "username": "Bosh",
+        "password": "password01"
+    },
+    {
+        "username": "Christi",
+        "password": "password02"
+    },
+    {
+        "username": "Cimmaron",
+        "password": "password03"
+    },
+    {
+        "username": "Dale",
+        "password": "password04"
+    },
+    {
+        "username": "Derek",
+        "password": "password05"
+    }
+]
