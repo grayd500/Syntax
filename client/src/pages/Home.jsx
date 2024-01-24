167375e1f3e676a53dd04a52dac3526f249d5a69
tugwellchristi <tugwellchristi@gmail.com>
2024-01-24 13:00:00 -0800

    modified homepage and other minor changes to various pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,38 +4,38 @@ import ReactAudioPlayer from 'react-audio-player';
 export default function Home() {
   const [isPlaying, setIsPlaying] = useState(false);
 
+
   const togglePlay = () => {
     setIsPlaying((prevIsPlaying) => !prevIsPlaying);
   };
 
   return (
-    <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <div style={{ display: 'flex', alignItems: 'flex-start', width: '100%', maxWidth: '800px', margin: 'auto' }}>
+    <div style={{ textAlign: 'center', maxWidth: '1250px', margin: '8rem auto 5rem', display: 'flex' }}>
+      <div style={{ flex: 1, marginTop: '40px' }}>
         <img
           src="/Band_3.png"
           alt='Animated 80s band image'
           style={{
             width: '100%',
             height: 'auto',
-            maxWidth: '600px',
-            display: 'block',
-            margin: 'auto',
-            marginTop: '150px',
+            maxWidth: '1100px',
             borderRadius: '10px',
           }}
         />
-        <div style={{ paddingLeft: '20px', flex: '1' }}>
-          <p style={{ color: '#E53179ff', fontSize: '18px' }}>
-            Check out our latest single "Binary Commencement Bytes" in #E53179ff.
-          </p>
-        </div>
       </div>
-      <ReactAudioPlayer
-        src="/night-city-knight.mp3"
-        autoPlay={isPlaying}
-        controls
-        style={{ width: '15%', marginTop: '20px' }}
-      />
+
+      <div style={{ flex: 1, marginLeft: '80px', textAlign: 'left', maxWidth: '600px', alignSelf: 'center' }}>
+        <p style={{ color: '#E53179ff', fontSize: '22px', marginBottom: '20px' }}>
+          Check out the latest single "Binary Commencement Bytes" from our album Neon Mountains, Volume 2!
+        </p>
+
+        <ReactAudioPlayer
+          src="/night-city-knight.mp3"
+          autoPlay={isPlaying}
+          controls
+          style={{ width: '50%', marginTop: '20px', marginLeft: '100px' }}
+        />
+      </div>
     </div>
   );
 }
--- client/src/pages/Tour.jsx
+++ client/src/pages/Tour.jsx
@@ -7,6 +7,22 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+
+const GeneralContainer = styled('div')({
+  marginTop: '8rem',
+  marginBottom: '1rem',
+});
+const StyledTableContainer = styled(TableContainer)({
+  margin: "auto",
+  maxWidth: "70%",
+  overflowX: "auto",
+  marginBottom: "40px",
+});
+
+const StyledTable = styled(Table)({
+  minWidth: 300,
+});
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -31,37 +47,26 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-const StyledTableContainer = styled(TableContainer)({
-  margin: "auto",
-  maxWidth: "90%",
-  overfloxX: "auto",
-  marginBottom: "40px",
-  
-});
-
-const StyledTable = styled(Table)({
-  minWidth: 300,
-});
-
-function createData(description, date, location, venue, tickets) {
-  return { description, date, location, venue, tickets };
+function createData(id, description, date, location, venue, tickets) {
+  return {id, description, date, location, venue, tickets };
 }
 
 const rows = [
-  createData('Digital Dreamscape Festival', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
-  createData('Digital Dreamscape Festival', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
-  createData('Digital Dreamscape Festival', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
-  createData('Digital Dreamscape Festival', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
-  createData('Digital Dreamscape Festival', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
-  createData('Digital Dreamscape Festival', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
-  createData('Digital Dreamscape Festival', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
-  createData('Digital Dreamscape Festival', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
-  createData('Digital Dreamscape Festival', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
+  createData(1, 'Digital Dreamscape Festival1', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
+  createData(2, 'Digital Dreamscape Festival2', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
+  createData(3, 'Digital Dreamscape Festival3', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
+  createData(4, 'Digital Dreamscape Festival4', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
+  createData(5, 'Digital Dreamscape Festival5', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
+  createData(6,'Digital Dreamscape Festival6', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
+  createData(7, 'Digital Dreamscape Festival7', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
+  createData(8, 'Digital Dreamscape Festival8', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
+  createData(9, 'Digital Dreamscape Festival9', 'June 12, 2024', 'Austin, Texas', 'Zilker Park', 'https://frontgatetickets.com'),
 ];
 
 export default function Tour() {
   return (
-    <StyledTableContainer component={Paper} style={{ marginTop: '8rem', marginBottom: '11rem' }}>
+    <GeneralContainer>
+    <StyledTableContainer component={Paper} >
       <StyledTable aria-label="customized table">
         <TableHead>
           <TableRow>
@@ -74,18 +79,25 @@ export default function Tour() {
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <StyledTableRow key={row.title}>
+            <StyledTableRow key={row.description}>
               <StyledTableCell component="th" scope="row">
                 {row.description}
               </StyledTableCell>
               <StyledTableCell align="right">{row.date}</StyledTableCell>
               <StyledTableCell align="right">{row.location}</StyledTableCell>
               <StyledTableCell align="right">{row.venue}</StyledTableCell>
-              <StyledTableCell align="right">{row.tickets}</StyledTableCell>
+              <StyledTableCell align="right">
+              <a href={row.tickets} target="_blank" rel="noopener noreferrer">
+              <Button variant="contained" style={{ backgroundColor: '#241742ff', color: 'white' }}>
+                    Get Tickets
+                  </Button>
+                  </a>
+                </StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
       </StyledTable>
     </StyledTableContainer>
+    </GeneralContainer>
   );
 }
