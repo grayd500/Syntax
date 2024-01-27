96bca6067a583f34d8ccc2d7c3492fd7fb95ee3f
cjenschke <cimarron13@mac.com>
2024-01-26 19:16:33 -0600

    user fixes to run query to register

--- server/models/user.js
+++ server/models/user.js
@@ -1,30 +1,47 @@
 // server/models/user.js:
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   firstName: {
-    type: String
+    type: String,
   },
   lastName: {
-    type: String
+    type: String,
   },
   email: {
-    type: String
+    type: String,
   },
   isAdmin: {
     type: Boolean,
-    default: false
+    default: false,
   },
 });
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+  }
+  next();
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+  return token;
+};
 
 const User = model('User', userSchema);
 
