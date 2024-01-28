48be5e1750ba21ad65f5e58e6e0a30597d778111
tugwellchristi <tugwellchristi@gmail.com>
2024-01-28 09:15:46 -0800

    cleaned up navbar and footer, better responisve UI

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -11,29 +11,41 @@ export default function Footer() {
     const iconColor = '#E53179ff';
 
     return (
-        <footer className="bg-black md:sticky top-0 z-10">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+        <footer className="bg-black md:sticky top-0 z-10 p-2 md:p-5">
+            <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center justify-between">
                 <div className="md:w-full text-center">
-                    <p className="text-base mt-4" style={{ color: textColor }}>
+                    <p className="text-base" style={{ color: textColor }}>
                         Let's Get Social:
                     </p>
                 </div>
                 <div className="md:w-full text-center mt-4">
                     <nav className="md:mx-auto md:py-1 md:pl-4 md:border-gray-700 flex flex-wrap items-center text-base justify-center">
                         {/* GitHub Icon */}
-                        <a href="https://github.com/grayd500/Syntax" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-white">
+                        <a href="https://github.com/grayd500/Syntax"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mr-5 hover:text-white">
                             <FaGithubSquare size={32} style={{ color: iconColor }} />
                         </a>
                         {/* LinkedIn Icon */}
-                        <a href="https://www.linkedin.com/pulse/topics/home/?trk=guest_homepage-basic_guest_nav_menu_articles" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-white">
+                        <a href="https://www.linkedin.com/pulse/topics/home/?trk=guest_homepage-basic_guest_nav_menu_articles"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mr-5 hover:text-white">
                             <FaLinkedin size={32} style={{ color: iconColor }} />
                         </a>
                         {/* Spotify Icon */}
-                        <a href="https://open.spotify.com/playlist/37i9dQZF1EIcwQjfM8Wvly" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-white">
+                        <a href="https://open.spotify.com/playlist/37i9dQZF1EIcwQjfM8Wvly"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mr-5 hover:text-white">
                             <FaSpotify size={32} style={{ color: iconColor }} />
                         </a>
                         {/* Discord Icon */}
-                        <a href="https://discord.com/" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-white">
+                        <a href="https://discord.com/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mr-5 hover:text-white">
                             <FaDiscord size={32} style={{ color: iconColor }} />
                         </a>
                     </nav>
